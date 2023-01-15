f4d5603403fd013d7bb9bdd04dcfa3576c4bcfe8
nat7216 <xukchen@ucm.es>
2023-01-15 15:36:32 +0100

    primer commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Server Sent Event Demo
+
+## Tabla de contenidos
+
+- [Server Sent Event Demo](#server-sent-event-demo)
+  - [Tabla de contenidos](#tabla-de-contenidos)
+  - [Intro ](#intro-)
+  - [Getting\_started ](#getting_started-)
+    - [Requisitos](#requisitos)
+    - [Instalación](#instalación)
+  - [Uso ](#uso-)
+
+## Intro <a name = "intro"></a>
+
+Demo para mostrar el funcionamiento de SSE y su implementación mediante javascript
+
+## Getting_started <a name = "getting_started"></a>
+
+
+### Requisitos
+
+- Node.JS
+- Navegador que soporte *EventSource*
+
+### Instalación
+
+```
+git clone 
+```
+
+## Uso <a name = "uso"></a>
+
+
+```
+>> cd /ruta/del/proyecto
+
+>> node server.js
+```
+Abrir el navegador y acceder a la http://localhost:8083, y F12 para ver los logs por la consola
+
+
+
+
---
+++ server.js
@@ -0,0 +1,66 @@
+let http = require("http");
+let util = require("util");
+let fs = require("fs");
+
+http.createServer(function (req, res) {
+  let acceptHeader = req.headers.accept.toLowerCase();
+  let url = req.url.toLowerCase();
+
+  if (url === "/eventos" && acceptHeader === "text/event-stream") {
+    handleSSE(res);
+  } else if (url === "/index.html" || url === "/") {
+    handleStatico(res, "index.html");
+  } else {
+    notFound(res);
+  }
+
+}).listen(8083);
+
+let notFound = function (res) {
+  res.writeHead(404);
+  res.end();
+};
+
+let timerId;
+let count = 0;
+
+let scheduleEvent = function (res) {
+  timerId = setTimeout(function () {
+    try {
+      emitRandomEvent(res);
+      scheduleEvent(res);
+    } catch (e) {
+      console.log("ERROR: " + e.message);
+    }
+  }, 5000);
+};
+
+let handleSSE = function (res) {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+  });
+
+  res.on("close", function () {
+    clearTimeout(timerId);
+  });
+
+  scheduleEvent(res);
+};
+
+let emitRandomEvent = function (res) {
+  let eventMsg = Math.random() < 0.5,
+    data = (new Date()).toISOString();
+  res.write("id: " + data + "\n");
+  if (eventMsg) {
+    res.write("event: evento\n");
+  }
+  res.write("data: " + data + "\n\n");
+};
+
+let handleStatico = function (res, fileName) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(fs.readFileSync(fileName));
+  res.end();
+};
